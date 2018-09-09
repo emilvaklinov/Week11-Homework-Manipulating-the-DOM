92c07a7864e52f4c6f724e47cb266d5149acd8ca
trakieca <emil_vaklinov@yahoo.com>
2018-09-09 23:56:13 +0100

    MVP and extentions done with new updates

--- js/app.js
+++ js/app.js
@@ -13,14 +13,15 @@ const handleNewVisitorFormSubmit = function (event) {
   const lastName = event.target.last_name.value
   const country = event.target.country.value
   const age = event.target.age.value
+  const transport = event.target.transport.value
   const gender = event.target.gender.value
   const payment = event.target.payment.value
   const date = event.target.date.value
   // const time = event.target.txt.value
 
 
-  paragraphResult.textContent = `First Name: ${firstName} | Last Name: ${lastName} |
-  Country: ${country} | Age: ${age} | Gender: ${gender} | Payment: ${payment} | Date: ${date};`
+  paragraphResult.textContent = `Name: ${firstName} ${lastName} |
+  Country: ${country} | Age: ${age} | Transport: ${transport} | Gender: ${gender} | Payment: ${payment} | Date: ${date};`
   const added = document.querySelector('#added');
   added.appendChild(paragraphResult);
   document.querySelector('#new-visitor-form');
